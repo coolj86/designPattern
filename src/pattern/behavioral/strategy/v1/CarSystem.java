7abeccac1ccd7e94aa8ca5a9bdd59693b9bcf036
whist <whist@DESKTOP-FDUAU19>
2023-02-10 21:44:20 +0900

    고쳤다

--- src/pattern/behavioral/strategy/v1/CarSystem.java
+++ src/pattern/behavioral/strategy/v1/CarSystem.java
@@ -16,5 +16,6 @@ public class CarSystem {
         CarSystem cs = new CarSystem();
         // 안전 장치 동작을 위한 모드는 센서 시스템쪽에서 해당 조건이 되면 전달한다고 가정한다.
         cs.saftyAction("case2");
+        System.out.println("고쳤다");
     }
 }
